8b3a44a11411ffaa846862db370039de43194e0b
Fabiano Dutra <fdutra.oliveira@gmail.com>
2020-06-22 22:16:12 -0300

    Criada tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+# Criei esta classe para definir os campos da tabela que serão exibidos no resumo da minha agenda
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuario', 'data_evento',)  # importante deixar a vírgula no final dentro dos parenteses
+
+admin.site.register(Evento, EventoAdmin)  # associando a classe EventoAdmin a classe Evento para que rode
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Para a agenda funcionar preciso de uma tabela para manipular esta agenda
+# a tabela terá informações sobre o evento como: título, data, data de criação
+# Então será criada a tabela Eventos
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)  # Charfield título, com tamanho 100, não pode ser em branco
+    descricao = models.TextField(blank=True, null=True)  # não tem tamanho limite, mas pode ser branco ou nulo
+    data_evento = models.DateTimeField(verbose_name="Data do Evento")  # não pode ser nulo, então fica sem parâmetro null
+    data_criacao = models.DateTimeField(auto_now=True)  # com este parâmetro auto_now é automático, não será preenchido
+                                                        # pelo usuário, ele sempre irá preencher com a hora atualizadapy
+    # Os eventos precisam de um usuário para identificar seu dono
+    # então será usada a tabela de usuários criada no primeiro exercício
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # usando um usuário estrangeiro, o cascade é para qdo
+                                                                # for deletar o usuário tb delete as outras infos dele
+    class Meta:
+        db_table = 'evento'  # está exigindo que minha tabela se chame evento
